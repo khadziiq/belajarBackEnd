77addbc01f480fda1ab760048d676dc63ec25cbb
khoirulkahf <fahrulista@gmail.com>
2022-01-13 10:13:38 +0700

    CRUD

--- postgres-sequelize/controllers/TodoController.js
+++ postgres-sequelize/controllers/TodoController.js
@@ -1,17 +1,44 @@
+const {todo} = require('../models')
+
 class TodoController{
     static readTodos(req, res){
-        res.json({
-            message:"Todos Page"
-        })
+            todo.findAll()
+            .then(todos=>{
+                res.json(todos)
+            })
+            .catch(err =>{
+                res.json(err)
+            })
     }
     static addTodo(req, res){
-        res.json({
-            message:"Add Todos Page"
+        let {task, status} = req.body
+        status === "true" ? status = true: status = false
+        todo.create({
+            task,
+            status
+        })
+        .then(todos=>{
+            res.json(todos)
+        })
+        .catch(err =>{
+            res.json(err)
         })
     }
     static findById(req, res){
-        res.json({
-            message:"Find By Id  Page"
+        const id = +req.params.id
+        todo.findByPk(id)
+        .then(result=>{
+            if (result) {
+                res.json(result) 
+            } else{
+                res.json({
+                    message:'Task Not Found'
+                })
+                
+            }
+        })
+        .catch(err =>{
+            res.json(err)
         })
     }
 }
